439c0bded929906da192472328425f019118acf6
AYIDouble <alpaypython@gmail.com>
2019-08-10 08:39:05 +0200

    🔁 Edit C++

--- ReadAndWriteMemory/ReadAndWriteMemory/ReadAndWriteMemory.cpp
+++ ReadAndWriteMemory/ReadAndWriteMemory/ReadAndWriteMemory.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
 
-	int readTest = 0;
+	int readTest = 0; // We store the Value we read from the Process here
 
 	// HWND (Windows window) by Window Name
 	HWND hwnd = FindWindowA(NULL, "Tutorial-x86_64");
@@ -18,14 +18,14 @@ int main(){
 	} else {
 		DWORD procID; // A 32-bit unsigned integer, DWORDS are mostly used to store Hexadecimal Addresses
 		GetWindowThreadProcessId(hwnd, &procID); // Getting our Process ID, as an ex. like 000027AC
-		HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procID);
+		HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procID); // Opening the Process with All Access
 
 		if (procID == NULL) {
 			cout << "Can't find Process." << endl;
 			Sleep(3000); // Sleep 3 seconds
 			exit(-1); // Exit the program if it did not find the Window
 		} else {
-			ReadProcessMemory(handle, (PBYTE*)0x03007640, &readTest, sizeof(readTest), 0); // Read the Process Memory
+			ReadProcessMemory(handle, (PBYTE*)0x03007640, &readTest, sizeof(readTest), 0); // Read the Process Memory, 03007640 is the Address
 			cout << readTest << endl;
 			Sleep(10000); // Sleep 10 seconds
 		}
